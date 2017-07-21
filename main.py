9777cabd39b53460345f6fa4b69b73da8d951554
YuanMing Liu <774166816@qq.com>
2017-07-21 18:55:28 +0800

    changes

--- main.py
+++ main.py
@@ -29,7 +29,6 @@ def parse_url_to_html(url):
     title_tag.string = title
     center_tag.insert(0, title_tag)
     body.insert(0, center_tag)
-
     html = str(body)
     html = html_template.format(content=html)
     html = html.encode("utf-8")
@@ -61,14 +60,16 @@ def save_pdf(htmls):
     }
     pdfkit.from_file(htmls, "Python_Tutorial.pdf", options=options)
 
-htmls = []
+if __name__ == '__main__':
 
-# Tips: 需要使用enumerate标注每个url对应的index，同时用join命名文件名，存为一个新的文件
-for index, url in enumerate(get_url_list()):
-    html = parse_url_to_html(url)
-    f_name = ".".join([str(index), "html"])
-    with open(f_name, 'wb') as f:
-        f.write(html)
-    htmls.append(f_name)
+    htmls = []
 
-save_pdf(htmls)
+    # Tips: 需要使用enumerate标注每个url对应的index，同时用join命名文件名，存为一个新的文件
+    for index, url in enumerate(get_url_list()):
+        html = parse_url_to_html(url)
+        f_name = ".".join([str(index), "html"])
+        with open(f_name, 'wb') as f:
+            f.write(html)
+        htmls.append(f_name)
+
+    save_pdf(htmls)
